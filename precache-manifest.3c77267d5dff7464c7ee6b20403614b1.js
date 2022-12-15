26941706f9dea5acda2da2cd1e3c24d4a99720c3
SprIoan <spiroioannou@gmail.com>
2022-12-15 11:48:20 +0200

    Updates

---
+++ precache-manifest.3c77267d5dff7464c7ee6b20403614b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fb4d6b381ef8f8d081c",
+    "url": "/robofriends/static/css/main.6a0a6861.chunk.css"
+  },
+  {
+    "revision": "3fb4d6b381ef8f8d081c",
+    "url": "/robofriends/static/js/main.3fb4d6b3.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robofriends/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "99ffb893002142636b1c",
+    "url": "/robofriends/static/css/2.52f73918.chunk.css"
+  },
+  {
+    "revision": "99ffb893002142636b1c",
+    "url": "/robofriends/static/js/2.99ffb893.chunk.js"
+  },
+  {
+    "revision": "6bf665704170461feaf8",
+    "url": "/robofriends/static/js/runtime~main.6bf66570.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d3753784a446ac602cb9e6651d9dd58d",
+    "url": "/robofriends/index.html"
+  }
+];
